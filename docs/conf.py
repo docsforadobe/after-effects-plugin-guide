4da9a2b820b5e7c9c21ae3aa3d9c9245ef402ad2
Zack Lovatt <zack@zacklovatt.com>
2018-10-05 11:40:25 -0700

    Initial commit

---
+++ docs/conf.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import sphinx_rtd_theme
+#from better import better_theme_path
+
+# -- General configuration ------------------------------------------------
+
+def setup(app):
+    app.add_stylesheet('extra.css')
+
+templates_path = ['_templates']
+
+source_suffix = '.rst'
+
+master_doc = 'index'
+
+# General information about the project.
+project = u'After Effects CC SDK Guide'
+copyright = u'1992–2017 Adobe Systems Incorporated'
+author = u'Adobe Systems Incorporated'
+
+version = u'16.0.0'
+release = u'1.0.0'
+
+pygments_style = 'sphinx'
+
+highlight_language = "javascript"
+
+# -- Options for HTML output ----------------------------------------------
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_static_path = ['_static']
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'ae-sdk-guide'
