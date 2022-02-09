3ec7cc1eb27c42406ea6876d1166d4876bbd47a0
Klustre <subs@klustre.nl>
2022-02-09 10:53:02 +0100

    Add dark mode toggle

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,10 @@ source_suffix = '.rst'
 
 master_doc = 'index'
 
+extensions = ['sphinx_rtd_dark_mode']
+
+default_dark_mode = False
+
 # General information about the project.
 project = u'After Effects SDK Guide'
 copyright = u'1992–2021 Adobe'
